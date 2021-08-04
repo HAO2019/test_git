ab2abff153dcc2bb1922090f87d011899cc9db1e
hao <867766817@qq.com>
2021-08-04 22:34:18 +0800

    测试

--- common/readconf.py
+++ common/readconf.py
@@ -3,5 +3,5 @@ import configparser
 class  DoConf():
     def doconf(self,section,option):
         con=configparser.ConfigParser()
-        haoa=con.get(section,option)
-        return haoa
+        hao=con.get(section,option)
+        return hao
