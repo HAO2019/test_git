4c195c1e2fe9b5c969e8955233d0ac324e38dacc
hao <867766817@qq.com>
2021-08-04 22:12:42 +0800

    新增了几个文件

--- common/readconf.py
+++ common/readconf.py
@@ -1,1 +1,7 @@
 #测试一下
+import configparser
+class  DoConf():
+    def doconf(self,section,option):
+        con=configparser.ConfigParser()
+        data=con.get(section,option)
+        return data
